9e255c45b8f38c6ca3c2d8ec3be20e60b28339eb
Leandro Gutierrez <lgutierrez@onebittech.com>
2023-11-06 12:49:57 +0100

    fix: corrijo deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leandrogutierrez148/lgutierrez.io.git master:gh-pages
+git push -f git@github.com:leandrogutierrez148/leandrogutierrez148.github.io.git master
 
 cd -
