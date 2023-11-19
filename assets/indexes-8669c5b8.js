d361d137f67fdb99dee5433abc27b6fc83c0cd36
leandrogutierrez148 <92802518+leandrogutierrez148@users.noreply.github.com>
2023-11-19 22:37:03 +0000

    Deploying to gh-pages from @ leandrogutierrez148/leandrogutierrez148.github.io@7c2976497e0686a828e2201a86451662885ea2f8 🚀

--- assets/indexes-8669c5b8.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/images/row-oriented.gif",e="/assets/images/column-oriented.gif",t="/assets/images/primarykeys.jpeg",a="/assets/images/indexes.png";export{s as _,e as a,t as b,a as c};
